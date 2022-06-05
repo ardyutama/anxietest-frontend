c1b4193aa890e5779ccb243230651b4236b0183b
Ardy Utama <ardy.putra@pinhome.id>
2022-06-05 23:09:01 +0700

    Clean some code  #3

--- src/components/about-us/index.jsx
+++ src/components/about-us/index.jsx
@@ -1,16 +1,7 @@
 import { Box, Heading } from "@chakra-ui/react";
 import Accordion from "../Accordion";
-import Content from "../Content/about-anxiety.json";
 
 const AboutUs = () => {
-//   const ShowAccordion = () => {
-//     const data = Content.data.AboutAnxiety;
-//     return data.map((item, index) => {
-//       return (
-//         <Accordion key={index} title={item.title} content={item.content} />
-//       );
-//     });
-//   };
   return (
     <Box display="flex" flexDirection="column" alignItems="center" mt={24} w={640}>
       <Heading
