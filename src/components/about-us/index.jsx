d81ac4706e51f1d2c913921691a823fc860ee57c
Ardy Utama <ardy.putra@pinhome.id>
2022-06-05 23:08:11 +0700

    Fix bug accordion and add title Fixes #3

--- src/components/about-us/index.jsx
+++ src/components/about-us/index.jsx
@@ -1,22 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import Accordion from "../Accordion";
 import Content from "../Content/about-anxiety.json";
 
 const AboutUs = () => {
-
-    const ShowAccordion = () => {
-        const data = Content.data.AboutAnxiety;
-        return data.map((item, index) => {
-            return (
-                <Accordion key={index} title={item.title} content={item.content} />
-            );
-        });
-    }
-    return(
-        <Box display="flex" flexDirection="column" w={640} gap={4}>
-            <ShowAccordion />
-        </Box>
-    );
+//   const ShowAccordion = () => {
+//     const data = Content.data.AboutAnxiety;
+//     return data.map((item, index) => {
+//       return (
+//         <Accordion key={index} title={item.title} content={item.content} />
+//       );
+//     });
+//   };
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" mt={24} w={640}>
+      <Heading
+        as="h1"
+        fontSize="32px"
+        fontWeight="bold"
+        textAlign="center"
+        maxW={460}
+        pb={10}
+      >Jenis Gangguan Kecemasan yang Umum Terjadi.</Heading>
+        <Accordion />
+    </Box>
+  );
 };
 
 export default AboutUs;
