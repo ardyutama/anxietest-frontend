7cecb6d8dcb5ebda8e64c5239dd18720326cf08e
Ardy Utama <ardy.putra@pinhome.id>
2022-06-06 20:28:35 +0700

    remove windows size utils

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -1,6 +1,7 @@
 import { Container } from "@chakra-ui/react";
 import HeroBanner from "../../components/HeroBanner";
 import AboutUs from "../../components/about-us";
+
 const LandingPage = () => {
   return (
     <Container display="flex" flexDirection="column" alignItems="center" maxW="-moz-max-content" px={16}>
