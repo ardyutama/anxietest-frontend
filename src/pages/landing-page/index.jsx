bef4e53e0ab8eb4f682ad07bcba99d42f473273c
ardy putra utama <ardy.putrautama53@gmail.com>
2022-06-12 03:17:11 -0700

    feat: Create Component - CTA (#32)
    Co-authored-by: Ardy Utama

--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -2,12 +2,14 @@ import { Container } from "@chakra-ui/react";
 import HeroBanner from "../../components/HeroBanner";
 import AboutUs from "../../components/about-us";
 import HowItWorks from "../../components/how-it-works";
+import { CTA } from "../../components/CTA";
 const LandingPage = () => {
   return (
     <Container display="flex" flexDirection="column" alignItems="center" maxW="-moz-max-content" px={16}>
       <HeroBanner />
       <AboutUs />
       <HowItWorks />
+      <CTA />
     </Container>
   );
 };
